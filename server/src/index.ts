acda8db6dae37f41382bbf43878bbc7b11ae7b30
carvpablo <plemosc480@gmail.com>
2026-07-21 17:41:00 -0300

    Prepara para deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,8 +11,15 @@ const app = express();
 const PORT = process.env.PORT || 3333;
 
 // Middlewares
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:5174',
+  'http://localhost:3000',
+  process.env.CLIENT_URL,
+].filter(Boolean) as string[];
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: process.env.CLIENT_URL ? allowedOrigins : '*',
   credentials: true,
 }));
 app.use(express.json());
